ea4f8b640605a25cb398c007d66846d6a3e609bf
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-09-15 01:06:15 +0530

    created schema for employee

---
+++ models/employee.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    profilePic: {
+        type: String,
+        required: true,
+        default: ""
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female"]
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /\d{10}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
+    designation:{
+        type: String,
+        required: true,
+    },
+    course:{
+        type: String,
+        required:true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    
+},{timestamps: true});
+
+const Employee = mongoose.model("Employee",employeeSchema);
+export default Employee;
