65045e358e65754d9a3daa3fa8daf7d60046f09a
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-09-17 02:52:01 +0530

    handel cors origin

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectToMongoDB from "./DB/connectToMongoDB.js";
 import adminAuthRoute from "./routes/admin.auth.js"
 import employeeAuthRoute from "./routes/employee.auth.js"
 
 const app = express();
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({ origin:"http://localhost:5173", credentials: true }));
 
 app.use('/api/adminauth', adminAuthRoute);
 app.use('/api/employeeauth', employeeAuthRoute);
