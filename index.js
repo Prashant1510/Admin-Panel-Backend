39ce3e22a407fd8cedbb6f44c229741e193b0b65
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-09-17 13:43:31 +0530

    change cors setting

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies to be sent
-  }));
+}));
 
 app.use('/api/adminauth', adminAuthRoute);
 app.use('/api/employeeauth', employeeAuthRoute);
