f58dd680ce8dcfbdb31ca130a8c301c897002e37
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-09-17 10:22:01 +0530

    correct cors origin error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin:"https://admin-panel-dealsdray.netlify.app/", credentials: true }));
+app.use(cors({ origin:"https://admin-panel-dealsdray.netlify.app", credentials: true }));
 
 app.use('/api/adminauth', adminAuthRoute);
 app.use('/api/employeeauth', employeeAuthRoute);
